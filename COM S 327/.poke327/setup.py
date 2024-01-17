0fba6e5240424424c8560f0d2456987bdd175bd8
Dylan <dbooth05@iastate.edu>
2024-01-17 10:13:20 -0600

    Upload of pokemon database for 327

---
+++ COM S 327/.poke327/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Pokedex',
+    version='0.1',
+    zip_safe=False,
+    packages=find_packages(),
+    package_data={
+        'pokedex': ['data/csv/*.csv']
+    },
+    install_requires=[
+        'SQLAlchemy>=1.0,<1.4',
+        'whoosh>=2.5,<2.7',
+        'markdown>=2.4.1,<=2.6.11',
+        'construct==2.5.3',
+        'six>=1.9.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pokedex = pokedex.main:setuptools_entry',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ]
+)
--- COM S 327/README.md
+++ COM S 327/README.md
@@ -5,3 +5,5 @@ This class over the course of the semester developed a version of Pokemon. The p
 There is more specific information about controls and features for each assignment in the individual README files. The final project is under the booth_dylan.assignment-1.10-2 directory. 
 
 In order to run the project, simply clone or download repository, then using the terminal go to directory where cloned repo is and open folder of project you wish to run. Compile project by running the command "make" and then once compiled run command "./poke327".
+
+You will also need to download the .poke327 directory, this directory contains all databases and correct paths to find all pokemon information for the project.
